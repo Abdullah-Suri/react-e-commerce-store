355f14462601aa6ef96d58e0bf1767b6c99689c3
ArsalanNewl <arsalan.pervez@newl.ca>
2024-11-25 20:17:45 +0500

    checkout + mainCart and footer css adding path in main jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer className="bg-white border-y">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <div className="mx-auto w-full container p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between gap-[192px]">
           <div className="mb-6 md:mb-0 ">
             <Link to="/" className="flex items-center text-black-900 text-2xl font-extrabold mb-[46px]">
